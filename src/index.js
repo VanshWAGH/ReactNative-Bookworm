0282b4fa9f21e269e10cfde13e007138845329b2
VanshWAGH <waghvansh0@gmail.com>
2025-06-09 17:33:27 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config"
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
